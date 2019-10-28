3eb3585f9e68c054129155ec3e0aae3c4a37f9b6
Kevin <kevinkumar90@gmail.com>
2019-10-28 13:52:58 -0700

    Add LC 130

--- 130_Surrounded_Regions.js
+++ 130_Surrounded_Regions.js
@@ -25,10 +25,9 @@ var solve = function (board) {
 }
 
 function dfs(board, i, j) {
-    if (i >= 0 && i < board.length && j >= 0
-        && j < board[i].length && board[i][j] === 'O') {
+    if (i >= 0 && i < board.length && j >= 0 && j < board[i].length && board[i][j] === 'O') {
 
-        board[i][j] = 'W'
+        board[i][j] = 'W';
 
         dfs(board, i + 1, j);
         dfs(board, i - 1, j);
