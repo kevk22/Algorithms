5026ca881b4440f2996dec71ae52844ec4f97879
Kevin <kevinkumar90@gmail.com>
2019-10-28 13:57:40 -0700

    Add comments on LC 120

--- 130_Surrounded_Regions.js
+++ 130_Surrounded_Regions.js
@@ -3,6 +3,7 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function (board) {
+    //Iterate through edges and set connected regions to 'W'
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             if (board[i][j] === 'O' && (i === 0 || i === board.length - 1 || j === 0 || j === board[i].length - 1)) {
@@ -11,6 +12,7 @@ var solve = function (board) {
         }
     }
 
+    //Iterate through board, set connected edge regions to 'O', set rest of regions to 'X'
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             if (board[i][j] === 'W') {
@@ -25,9 +27,10 @@ var solve = function (board) {
 
 function dfs(board, i, j) {
     if (i >= 0 && i < board.length && j >= 0 && j < board[i].length && board[i][j] === 'O') {
-
+        //Mark all regions connected to an edge
         board[i][j] = 'W';
 
+        //DFS on all conected directly connected regions
         dfs(board, i + 1, j);
         dfs(board, i - 1, j);
         dfs(board, i, j + 1);
