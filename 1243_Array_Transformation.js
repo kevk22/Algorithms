f588153939ca5bd7719e91386f05a04f0e551d2d
Kevin <kevinkumar90@gmail.com>
2019-11-22 16:20:38 -0800

    Add LC 1243

---
+++ 1243_Array_Transformation.js
@@ -0,0 +1,22 @@
+/*
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var transformArray = function(arr) {
+    if (arr.length < 2) return arr;
+    
+    while (true) {
+      let prev = arr.slice();
+      for (let i = 1; i < arr.length - 1; i++) {
+        if (prev[i] > prev[i+1] && prev[i] > prev[i-1]) {
+          arr[i]--;
+        } else if (prev[i] < prev[i+1] && prev[i] < prev[i-1]) {
+          arr[i]++
+        }
+      }
+      
+      if (prev.join('') === arr.join('')) break;
+    }
+    
+  return arr;
+};
