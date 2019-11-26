50d2d1e829afb8ab12682a58d946cfe7d9907661
Kevin <kevinkumar90@gmail.com>
2019-11-26 01:02:22 -0800

    Add LC 21

--- 237_Delete_Node_Linked_List.js
+++ 237_Delete_Node_Linked_List.js
@@ -9,7 +9,31 @@
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
+
+// Only given node
 var deleteNode = function (node) {
     node.val = node.next.val;
     node.next = node.next.next;
 };
+
+
+// Given both node and head 
+function deleteNode(head, position) {
+
+    if (position === 0) return head.next;
+
+    let count = 0;
+    let cur = head;
+
+    while (count < position || !cur) {
+        if (count === position - 1) {
+            cur.next = cur.next.next || null;
+            return head;
+        }
+
+        cur = cur.next;
+        count++;
+    }
+
+    return head;
+}
