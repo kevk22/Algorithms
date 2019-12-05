e8c43a01471cc34f08bd064752b1c890eeb85c14
Kevin <kevinkumar90@gmail.com>
2019-12-05 01:42:57 -0800

    Update LC 73

--- 73_Set_Matrix_Zeroes.js
+++ 73_Set_Matrix_Zeroes.js
@@ -2,6 +2,46 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+
+// O(n) space using first row and column as flags
+var setZeroes = function (matrix) {
+    let colCheck = false;
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][0] === 0) colCheck = true;
+
+        for (let j = 1; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[i].length; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) matrix[i][j] = 0;
+        }
+    }
+
+    if (matrix[0][0] === 0) {
+        for (let i = 0; i < matrix[0].length; i++) {
+            matrix[0][i] = 0;
+        }
+    }
+
+    if (colCheck) {
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    return matrix;
+};
+
+
+
+// O(m + n) space 
 var setZeroes = function (matrix) {
     let check = {
         x: new Set(),
