989013b5fbf2e2bb9ec5570f7f8592d6bc4f6844
Kevin <kevinkumar90@gmail.com>
2019-12-21 14:14:19 -0800

    add LC 937

---
+++ 937_Reorder_Data_in_Log_Files.js
@@ -0,0 +1,31 @@
+/*
+ * @param {string[]} logs
+ * @return {string[]}
+ */
+var reorderLogFiles = function(logs) {
+    let letterLogs = [];
+    let digitLogs = [];
+    
+    // seperate letterlogs and digitlogs
+    for (let i = 0; i < logs.length; i++) {
+        let cur = logs[i].split(' ');
+        
+        if (!isNaN(cur[1])) {
+            digitLogs.push(cur.join(' '));
+        } else {
+            letterLogs.push(cur.join(' '));
+        }
+    }
+    
+    // sort letterlogs lexicographically
+    letterLogs.sort((a, b) => {
+        let strA = a.split(' ').slice(1).join(' ');
+        let strB = b.split(' ').slice(1).join(' ');
+        
+        if (strA === strB) return a.localCompare(b);
+        return strA.localeCompare(strB);
+    });
+    
+    // merge and return sorted arrays 
+    return [...letterLogs, ...digitLogs];
+};
