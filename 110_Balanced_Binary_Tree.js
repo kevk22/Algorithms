5f01b15dd33b90fbc4209c5243f8d80f7920b083
Kevin <kevinkumar90@gmail.com>
2020-01-06 19:46:50 -0800

    Add LC 110

---
+++ 110_Balanced_Binary_Tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/*
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+var isBalanced = function (root) {
+    if (!root) return true;
+
+    function getHeight(root) {
+        if (!root) return 0;
+
+        let left = getHeight(root.left);
+        let right = getHeight(root.right);
+
+        return 1 + Math.max(left, right);
+    }
+
+    let dif = Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1;
+
+    return dif && isBalanced(root.left) && isBalanced(root.right)
+};
