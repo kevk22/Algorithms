3e501105774055858b4bd02e3a5955f29900bac7
kevk22 <kevinkumar90@gmail.com>
2021-02-15 16:57:44 -0800

    Add LC 46

---
+++ 46_Permutations .js
@@ -0,0 +1,34 @@
+/*
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  let res = [];
+
+  if (!nums.length) return res;
+
+  let seen = new Array(nums.length).fill(false);
+  let permutation = [];
+
+  findPermutation(nums, permutation, seen, res);
+
+  return res;
+};
+
+var findPermutation = function (nums, permutation, seen, res) {
+  if (permutation.length === nums.length) {
+    res.push([...permutation]);
+    return;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (seen[i]) continue;
+
+    seen[i] = true;
+    permutation.push(nums[i]);
+    findPermutation(nums, permutation, seen, res);
+    permutation.pop();
+    seen[i] = false;
+  }
+
+}
